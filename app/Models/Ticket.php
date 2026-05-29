bd66e72ff225356f35eacf8ba84e61fac1d9f578
Ruslan <kucherukrn@gmail.com>
2026-05-29 23:51:33 +0200

    Integrate Media libary for Ticket attachments

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Ticket extends Model
 {
     /** @use HasFactory<\Database\Factories\TicketFactory> */
     use HasFactory;
+    use InteractsWithMedia;
 
     public function customer(){
         return $this->belongsTo(Customer::class);
